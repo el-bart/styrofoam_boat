2ab76dac109e0f38f9309901c9106cd50c78645d
bartek 'basz' szurgot <git@baszerr.eu>
2023-04-13 22:05:11 +0200

    added main engine PWM, too

--- boat/sw/src/main.cpp
+++ boat/sw/src/main.cpp
@@ -2,17 +2,21 @@
 #include <Servo.h>
 
 Servo servo;
+auto const pin_servo = 2;
+auto const pin_engine = 5;
 
 void setup()
 {
-  servo.attach(2);    // aka: D2
+  servo.attach(pin_servo);
+  pinMode(pin_engine, OUTPUT);
 }
 
 void loop()
 {
   for(auto i=0; i<180; ++i)
   {
-    servo.write(i);
-    delay(15);
+    analogWrite(pin_engine, i); // 1kHz PWM: 0-255
+    servo.write(i);             // angle in degrees: 0-180
+    delay(150);
   }
 }
