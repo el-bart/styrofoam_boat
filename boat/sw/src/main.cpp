8f3d2fdf272e3fb98fcf6b9d966019a7d0476cef
bartek 'basz' szurgot <git@baszerr.eu>
2023-04-16 19:46:14 +0200

    added start position on uC restart

--- boat/sw/src/main.cpp
+++ boat/sw/src/main.cpp
@@ -8,12 +8,32 @@ auto constexpr pin_servo = 2;
 auto constexpr pin_engine = 5;
 ProtoFsm fsm;
 
+namespace
+{
+void apply_outputs(uint8_t byte)
+{
+  if( not fsm.add_byte(byte) )
+    return;
+  auto const frame = fsm.pop();
+  // TODO: apply frame.engine_direction()
+  analogWrite( pin_engine, frame.engine_speed() );
+  servo.write( frame.rudder_angle() );
+}
+
+void start_position()
+{
+  apply_outputs(60);
+  apply_outputs(0);
+  apply_outputs((60^0) | 0x80);
+}
+}
+
 void setup()
 {
   Serial.begin(9600);   // RX == 0, TX == 1
   servo.attach(pin_servo);
   pinMode(pin_engine, OUTPUT);
-
+  start_position();
   {
     wdt_disable();
     // wdt_enable(WDTO_2S)
@@ -44,14 +64,6 @@ SerialByte read_serial()
     return {};
   Serial.write('x');
   return SerialByte{ static_cast<uint8_t>(byte) };
-}
-
-void apply_outputs(uint8_t byte)
-{
-  if( not fsm.add_byte(byte) )
-    return;
-  analogWrite(pin_engine, 0);
-  servo.write(90);
 }
 }
 
