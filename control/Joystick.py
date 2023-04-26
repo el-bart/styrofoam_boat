56fd50090db359c2a53427d643ce9a08bb83420a
bartek 'basz' szurgot <git@baszerr.eu>
2023-04-26 19:03:14 +0200

    added mapping from raw throttle values to 75-100 range, that is actually 'runnable' on the engine

--- control/Joystick.py
+++ control/Joystick.py
@@ -29,8 +29,20 @@ class Device:
         js_time, js_value, js_type, js_number = struct.unpack('IhBB', ev)
         if js_type & 0x02:
             if js_number == self._axis_throttle:
-                self._throttle = -int( ( js_value / 2**15 ) * 100 )
+                value = -int( ( js_value / 2**15 ) * 100 )
+                self._throttle = self._map_throttle(value)
                 return
             if js_number == self._axis_rudder:
                 self._rudder = int( ( js_value / 2**15 ) * 60 )
                 return
+
+    def _map_throttle(self, v):
+        abs_min_throttle = 75
+        abs_max_throttle = 100
+        delta = abs_max_throttle - abs_min_throttle
+        if v == 0:
+            return v
+        sign = -1 if v < 0 else +1
+        denom = abs_max_throttle / delta
+        out_fp = ( sign * abs_min_throttle ) + ( v / denom )
+        return int(out_fp)
